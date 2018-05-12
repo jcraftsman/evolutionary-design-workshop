311bcc7c3ac6a4da00ab16001400f92860708dca
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-05-12 16:59:35 +0200

    extract setup_iteration function + use iteration number as argument (1 by default)

--- start_workshop.sh
+++ start_workshop.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+readonly DEFAULT_ITERATION_NUMBER=1
 function main
 {
   check_command_exists "git" "Git command line tool is not installed in your computer. You need to install it."
+  iteration_number=${1:-$DEFAULT_ITERATION_NUMBER}
 
   set -o errexit
   set -o pipefail
@@ -9,9 +11,9 @@ function main
   set -o errtrace
 
   rm -rf .git
-  git clone https://github.com/jcraftsman/pictures-analyzer.git
-  cd pictures-analyzer
-  git checkout workshop-it1
+
+  setup_iteration ${iteration_number}
+
   echo ""
   echo ""
   echo "****************************************************************************************"
@@ -27,6 +29,14 @@ function check_command_exists
   command=$1
   message=$2
   command -v ${command} >/dev/null 2>&1 || { echo >&2 ${message}; exit 1; }
+}
+
+function setup_iteration
+{
+  iteration_number=$1
+  git clone https://github.com/jcraftsman/pictures-analyzer.git
+  cd pictures-analyzer
+  git checkout "workshop-it${iteration_number}"
 }
 
 main "$@"
