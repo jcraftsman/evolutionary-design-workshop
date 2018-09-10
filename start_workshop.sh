4bfc084fe33cec3efb9ec883cca9d447a82b57f0
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-09-10 12:12:16 -0600

    refactor language selection

--- start_workshop.sh
+++ start_workshop.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
 readonly SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd );
+readonly CODE_DIR_PREFIX="pictures-analyzer"
+readonly PYTHON_CODE_DIR="${SCRIPT_DIR}/${CODE_DIR_PREFIX}"
+readonly CODE_DIR_PATTERN="${SCRIPT_DIR}/${CODE_DIR_PREFIX}*"
 
 function main
 {
-  if [ -z "$(ls ${SCRIPT_DIR}/pictures-analyzer*)" ]; then
-    selected_language=$(ask_user_for_language)
-  elif [ -d ${SCRIPT_DIR}/pictures-analyzer ]; then
-    selected_language="python"
-  else
-    selected_language=$(ls ${SCRIPT_DIR} | grep "pictures-analyzer" | cut -d "-" -f 3)
-  fi
+  selected_language=$(select_language)
   workshop_starter_script="${SCRIPT_DIR}/${selected_language}/start_workshop.sh"
   bash ${workshop_starter_script} "$@"
+}
+
+function select_language
+{
+  select_language=""
+  if [ -d ${PYTHON_CODE_DIR} ]; then
+    selected_language="python"
+  elif ls ${CODE_DIR_PATTERN} 1> /dev/null 2>&1 ; then
+    selected_language=$(determine_language_from_code_dir)
+  else
+    selected_language=$(ask_user_for_language)
+  fi
+  echo ${selected_language}
 }
 
 function ask_user_for_language
@@ -32,6 +42,11 @@ function ask_user_for_language
     esac
   done
   echo ${selected_language}
+}
+
+function determine_language_from_code_dir
+{
+  echo $(ls ${SCRIPT_DIR} | grep ${CODE_DIR_PREFIX} | cut -d "-" -f 3) 
 }
 
 main "$@"
