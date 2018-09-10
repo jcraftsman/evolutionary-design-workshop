c8a7976e25c98eef536345c6f3c2a9e308ace5f8
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-09-10 11:10:09 -0600

    ask user for language only the first time, and then determine selected language

--- start_workshop.sh
+++ start_workshop.sh
@@ -4,12 +4,19 @@ readonly SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd );
 
 function main
 {
-  selected_language=$(ask_language)
+  if [ -z "$(ls ${SCRIPT_DIR}/pictures-analyzer*)" ]; then
+    selected_language=$(ask_user_for_language)
+  elif [ -d ${SCRIPT_DIR}/pictures-analyzer ]; then
+    selected_language="python"
+  else
+    selected_language=$(ls ${SCRIPT_DIR} | grep "pictures-analyzer" | cut -d "-" -f 3)
+  fi
   workshop_starter_script="${SCRIPT_DIR}/${selected_language}/start_workshop.sh"
   bash ${workshop_starter_script} "$@"
 }
 
-function ask_language() {
+function ask_user_for_language
+{
   selected_language=""
   while true; do
     read -p "Language (java, python) ?  " response
